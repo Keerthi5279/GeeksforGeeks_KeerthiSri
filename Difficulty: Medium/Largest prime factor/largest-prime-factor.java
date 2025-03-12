7a36f7f1750c0d1610baeba19020f48794f6e7d5
Keerthi5279 <keerthisri1232@gmail.com>
2025-03-12 13:56:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest prime factor/largest-prime-factor.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.largestPrimeFactor(N));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int largestPrimeFactor(int n) {
+        // code here
+        int max=Integer.MIN_VALUE;
+        for(int i = 2; i*i <= n; i++){
+            if(n%i==0){
+           
+                max=Math.max(max,i);
+            }
+            while(n%i==0){
+                 n=n/i;
+            }
+           
+        }
+        
+        if(n>1){
+            max=Math.max(max,n);
+        }
+        return max;
+            
+    }
+}
