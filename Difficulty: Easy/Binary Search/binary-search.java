70d78af6afd13b82a70f1a091daac10b2c42889a
Keerthi5279 <keerthisri1232@gmail.com>
2025-04-04 21:47:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Binary Search/binary-search.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // consume the newline
+        while (t-- > 0) {
+            int k = sc.nextInt();
+            sc.nextLine(); // consume the newline
+            String input = sc.nextLine();
+            String[] strNumbers = input.split(" ");
+            int[] arr = new int[strNumbers.length];
+            for (int i = 0; i < strNumbers.length; i++) {
+                arr[i] = Integer.parseInt(strNumbers[i]);
+            }
+            Solution ob = new Solution();
+            int res = ob.binarysearch(arr, k);
+            System.out.println(res);
+
+            System.out.println("~");
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int binarysearch(int[] arr, int k) {
+        // Code Here
+        
+        for (int i = 0; i < arr.length; i++) {  
+            if (arr[i] == k) {  
+                return i;  
+            }  
+        }  
+        return -1; 
+    }  
+
+    public static void main(String args[]) {  
+        Solution sol = new Solution();  
+
+        int arr[] = {53, 57, 100};  
+        int k = 6;  
+
+        System.out.print(sol.binarysearch(arr, k));  
+    }
+}
